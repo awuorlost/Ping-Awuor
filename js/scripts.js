b1550a2ab551fa6a3147cf03dde849992500ab7c
Awuor <awuorattyang@gmail.com>
2018-04-12 21:40:10 -0700

    add javascript for back and front end logic

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+var list=[]
+function PingPong(number){
+for (var i=1; i <=number ; i++)
+  {
+
+   if (i % 15 === 0)
+    var a="PingPong"
+   else if (i % 3 === 0)
+      a="Ping"
+   else if (i % 5 === 0)
+      a="Pong"
+   else
+     a=i
+
+    list.push(a)
+
+  }
+}
+
+
+//FRONT-END
+$(document).ready(function(){
+  $("#ping-form").submit(function(event){
+    event.preventDefault();
+    var userInput=parseInt($("#input-number").val());
+    PingPong(userInput);
+    list.forEach(function(element){
+    $("#result").append($("<li>").html(element));
+    })
+  });
+});
